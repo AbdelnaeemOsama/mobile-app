22dc6ed1f91ab6aadc68d752738a2f687cd6917f
AbdelnaeemOsama <125660203+AbdelnaeemOsama@users.noreply.github.com>
2024-12-16 15:01:08 +0200

    Update colors.dart

--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static Color primaryColor = Color(0XFF39298a);
-  static Color primaryColorDark = Color(0xFF231954);
+  static Color primaryColor = const Color(0XFF39298a);
+  static Color primaryColorDark = const Color(0xFF231954);
 }
